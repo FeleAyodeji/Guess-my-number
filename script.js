75cff404723afa182b9fcd36a142c33c03cd4e66
FeleAyodeji <feledeji@gmail.com>
2023-01-14 10:23:17 +0100

    adding neccessary comments and also fixed some bug

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ console.log(document.querySelector('.guess').value);
 
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
+let highscore = 0
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -25,7 +26,11 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('.number').textContent = secretNumber; // the secretnumber is displayed when the guess matches it
     document.querySelector('body').style.backgroundColor = '#60b347' //chnage the backgorund color to green when the player wins
     document.querySelector('.number').style.width = "30rem" // changes the width of the number box when the player wins
-   
+    if(score > highscore) {
+      highscore = score
+    }
+    document.querySelector('.highscore').textContent = highscore
+
     //when the player enters a number greater than the secretnumber
   } else if (guess > secretNumber) {
     if (score > 1) {
@@ -50,12 +55,14 @@ document.querySelector('.check').addEventListener('click', function () {
   }
 });
 
-document.querySelector('.again').addEventListener('click' , function(){
-  document.querySelector('.score').textContent = 20  
-  document.querySelector('.number').textContent = '?';
-  document.querySelector('.message').textContent = "Start guessing..."
-  let guess = document.querySelector('.guess').textContent = 
-  document.querySelector('body').style.backgroundColor = '#222'
+document.querySelector('.again').addEventListener('click', function(){ //the again button serves the reset function
+  score = 20
+  document.querySelector('.score').textContent = score //restoring the initial value of score
+  document.querySelector('.message').textContent = "Start guessing..." // retore the initial condition of message
+  document.querySelector('.guess').value = ''// restoring the initial condition of guess
+  document.querySelector('body').style.backgroundColor = '#222' // restoring the initial color of the background
+  document.querySelector('.number').style.width = "15rem" //restoring the initial width of the number box
+  document.querySelector('.number').textContent = '?'
 
   
 })
